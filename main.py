cdc70a162876ae13d3f7a35c3d5473ffe8c61834
07Smiley <bhushanchavan782006@eng.rizvi.edu.in>
2026-02-19 20:53:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # AIM: Write a Python program to calculate the gross salary of an employee.
 # Coder:
 # Date:
-basic = 5000.0
+print("Salary Calculator")
+
+basic = float(input("Enter basic salary: "))
+
 da = basic * 0.70
 ta = basic * 0.30
 hra = basic * 0.10
 gross = basic + da + ta + hra
+
 print("\nSalary Details:")
-print("Basic Salary:", basic)
-print("DA (70%):", da)
-print("TA (30%):", ta)
-print("HRA (10%):", hra)
-print("Gross Salary:", gross)
+print(f"Basic Salary: {basic:.1f}")
+print(f"DA (70%): {da:.1f}")
+print(f"TA (30%): {ta:.1f}")
+print(f"HRA (10%): {hra:.1f}")
+print(f"Gross Salary: {gross:.1f}")
 # Write your code here
