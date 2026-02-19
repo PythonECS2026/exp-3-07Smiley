0b306f9767ca3f921a1046894ab551229b506e93
07Smiley <bhushanchavan782006@eng.rizvi.edu.in>
2026-02-19 21:03:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ ta = basic * 0.30
 hra = basic * 0.10
 gross = basic + da + ta + hra
 print("Salary Details:")
-print(f"Basic Salary: {basic:.1f}")
-print(f"DA (70%): {da:.1f}")
-print(f"TA (30%): {ta:.1f}")
-print(f"HRA (10%): {hra:.1f}")
-print(f"Gross Salary: {gross:.1f}")
+print(f"Basic Salary:\t{basic:.1f}")
+print(f"DA (70%):\t{da:.1f}")
+print(f"TA (30%):\t{ta:.1f}")
+print(f"HRA (10%):\t{hra:.1f}")
+print(f"Gross Salary:\t{gross:.1f}"
 # Write your code here
